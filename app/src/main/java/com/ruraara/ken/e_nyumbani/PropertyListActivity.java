5d645b8c9375b28006fe5e25ba91733ceec38a03
kenedyivan <kenedyakenaivan@gmail.com>
2017-10-30 12:08:09 +0300

    Added splash screen activity and navigation drawer to PropertyListActivity

--- app/src/main/java/com/ruraara/ken/e_nyumbani/PropertyListActivity.java
+++ app/src/main/java/com/ruraara/ken/e_nyumbani/PropertyListActivity.java
@@ -6,6 +6,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -13,10 +17,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -42,7 +50,7 @@ import cz.msebera.android.httpclient.Header;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class PropertyListActivity extends AppCompatActivity {
+public class PropertyListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     String TAG = PropertyListActivity.class.getSimpleName();
 
@@ -54,14 +62,20 @@ public class PropertyListActivity extends AppCompatActivity {
 
     View recyclerView;
 
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_property_list);
+//        setContentView(R.layout.activity_property_list);
+        setContentView(R.layout.activity_drawer);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
+
+        progressBar = (ProgressBar) findViewById(R.id.indeterminateBar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +85,15 @@ public class PropertyListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         recyclerView = findViewById(R.id.property_list);
         assert recyclerView != null;
@@ -95,6 +118,7 @@ public class PropertyListActivity extends AppCompatActivity {
             public void onStart() {
                 // called before request is started
                 Log.d(TAG, "Started request");
+                progressBar.setVisibility(View.VISIBLE);
             }
 
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -121,7 +145,8 @@ public class PropertyListActivity extends AppCompatActivity {
                         String agent = jsonObject.getString("agent");
                         String price = jsonObject.getString("price");
                         String image = jsonObject.getString("image");
-                        Property.addPropertyItem(Property.createPropertyItem(String.valueOf(id), title, address, agent, price, image));
+                        Property.addPropertyItem(Property.createPropertyItem(String.valueOf(id),
+                                title, address, agent, price, image));
 
                     }
                 } catch (JSONException e) {
@@ -132,6 +157,8 @@ public class PropertyListActivity extends AppCompatActivity {
 
                 setupRecyclerView((RecyclerView) recyclerView);
 
+                progressBar.setVisibility(View.INVISIBLE);
+
                 //End work from here
 
             }
@@ -140,12 +167,14 @@ public class PropertyListActivity extends AppCompatActivity {
             public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
                 // called when response HTTP status is "4XX" (eg. 401, 403, 404)
                 Log.d(TAG, "failed " + statusCode);
+                Toast.makeText(PropertyListActivity.this,"Network error",Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRetry(int retryNo) {
                 // called when request is retried
                 Log.d(TAG, "retryNO: " + retryNo);
+                Toast.makeText(PropertyListActivity.this,"Taking too long",Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -234,5 +263,62 @@ public class PropertyListActivity extends AppCompatActivity {
                 return super.toString() + " '" + mTitleView.getText() + "'";
             }
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
